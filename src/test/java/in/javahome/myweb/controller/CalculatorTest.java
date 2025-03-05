024c0fc9dabce98a7d3f4ef8c68d07663128600e
Vutukuru Karthikeya Reddy <115484346+Karthikeyareddy81@users.noreply.github.com>
2025-03-05 12:39:16 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,14 +1,19 @@
 package in.javahome.myweb.controller;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorTest extends TestCase {
-	Calculator cal = new Calculator();
-	public void testAdd(){
-		Assert.assertEquals(cal.add(10, 20), 30);
-	}
-	public void testMultiply(){
-		Assert.assertEquals(cal.multiply(10, 20), 200);
-	}
+public class CalculatorTest {
+    
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testMultiplication() {
+        int result = 4 * 5;
+        assertEquals(20, result);
+    }
 }
